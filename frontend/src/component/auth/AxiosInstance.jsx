154d53315e81f67db77117cb6ecb9511345d2f48
collectiontrip <117537950+collectiontrip@users.noreply.github.com>
2024-12-30 19:55:39 +0530

    Update AxiosInstance.jsx

--- frontend/src/component/auth/AxiosInstance.jsx
+++ frontend/src/component/auth/AxiosInstance.jsx
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-// Create Axios instance
+
 const AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/', // Your backend base URL
-  timeout: 5000, // Optional: Set a timeout for requests
+  baseURL: 'http://127.0.0.1:8000/', 
+  timeout: 5000, 
 });
 
-// Add a request interceptor to include the JWT token dynamically
+
 AxiosInstance.interceptors.request.use(
   (config) => {
-    // Get the token from localStorage on each request
+    
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`; // Add token to headers
+      config.headers['Authorization'] = `Bearer ${token}`; 
     }
     return config;
   },
