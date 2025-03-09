47ace822ace399e75b4bb7eb67ced03624702cfc
Ravi Dochania <collectiontrip0@gmail.com>
2025-03-09 22:25:35 +0530

    prepare for production

--- collectiontrip/settings/dev.py
+++ collectiontrip/settings/dev.py
@@ -2,18 +2,26 @@ from .common import *
 
 DEBUG = True
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-0%&w+sj6pcw=6a%fh^$pt1_kw(pj1!e^at9=$40-#0-^os+o@1'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'NAME': 'collectiontrip3',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'Ravidoc1@1$',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
+
+TWILIO_ACCOUNT_SID = "ACaae3e97351f066031fdfc5dc48cdb661"
+TWILIO_AUTH_TOKEN = "2466d63b79c419a15e593f6e7a5b2d13"
+TWILIO_PHONE_NUMBER = "+15733754020"
+
 
 
 
--- collectiontrip/settings/prod.py
+++ collectiontrip/settings/prod.py
@@ -1,10 +1,10 @@
-import os
 from .common import *
 from .common import BASE_DIR
 
 
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
+#9^!)q2#9bj&x8ya1_597*x+&v#n(xd7phvk5x#ul&8oa#ic_x$
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
@@ -27,3 +27,7 @@ DATABASES = {
 }
 
 STATIC_ROOT = BASE_DIR/'static'
+
+TWILIO_ACCOUNT_SID = "ACaae3e97351f066031fdfc5dc48cdb661"
+TWILIO_AUTH_TOKEN = "2466d63b79c419a15e593f6e7a5b2d13"
+TWILIO_PHONE_NUMBER = "+15733754020"
